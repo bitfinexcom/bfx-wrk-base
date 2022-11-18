1ab2cdd157c02d8063fd1790351651b15b16a69e
Daniel1984 <aciddaniel@gmail.com>
2022-11-18 16:56:27 +0100

    pr feedback

--- test/base.unit.test.js
+++ test/base.unit.test.js
@@ -24,29 +24,25 @@ describe('loadConf', () => {
   it('should exit when config keys do not match example nested config keys', () => {
     const workerBase = new WrkBase({}, { root: __dirname })
     workerBase.loadConf('missing-nested-keys.coin', 'coin')
-    assert.ok(process.exit.called)
-    assert.ok(process.exit.calledWith(1))
+    assert.ok(process.exit.calledOnceWithExactly(1))
   })
 
   it('should exit if required value is missing', () => {
     const workerBase = new WrkBase({}, { root: __dirname })
     workerBase.loadConf('missing-values.coin', 'coin', { 'symbol': { required: true } })
-    assert.equal(process.exit.called, true)
-    assert.equal(process.exit.calledWith(1), true)
+    assert.ok(process.exit.calledOnceWithExactly(1))
   })
 
   it('should exit if cfg value not equal example value', () => {
     const workerBase = new WrkBase({}, { root: __dirname })
     workerBase.loadConf('value-mismatch.coin', 'coin', { 'sweepKeep': { sameAsExample: true } })
-    assert.equal(process.exit.called, true)
-    assert.equal(process.exit.calledWith(1), true)
+    assert.ok(process.exit.calledOnceWithExactly(1))
   })
 
   it('should exit if nested cfg value not equal nested example value', () => {
     const workerBase = new WrkBase({}, { root: __dirname })
     workerBase.loadConf('nested-value-mismatch.coin', 'coin', { 'root.lvl2.lvl3.value': { sameAsExample: true } })
-    assert.equal(process.exit.called, true)
-    assert.equal(process.exit.calledWith(1), true)
+    assert.ok(process.exit.calledOnceWithExactly(1))
   })
 
   it('should not exit when config keys match example config keys', () => {
